ed2f9519b22d028e35ceee5d14d2f028de154c77
Oliver Mao <olivermao2005@gmail.com>
2024-11-30 22:43:57 -0500

    fixed player PDCs not working

--- scripts/map/map_canvas.gd
+++ scripts/map/map_canvas.gd
@@ -11,5 +11,4 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("key_m"):
-		print("m")
 		visible = !visible
--- scripts/ship_parts/mcrn/pdc.gd
+++ scripts/ship_parts/mcrn/pdc.gd
@@ -7,6 +7,7 @@ var prevPDCTargetAngle = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	get_parent().pdcPivots.append(self)
 	pass # Replace with function body.
 
 func predictTime(target, targetPosition = null):
